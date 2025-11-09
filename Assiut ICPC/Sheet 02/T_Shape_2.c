3c94a89e8c83263bf10a290bd0aa7fa8f77a947f
Rabius $unny <rabiussunny7359@gmail.com>
2025-11-10 03:20:08 +0600

    Sheet 02 | T-Shape_2

---
+++ Assiut ICPC/Sheet 02/T_Shape_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int row,col;
+    for(row=1;row<=N;row++)
+    {
+        for( col=1;col<=N-row;col++)
+        {
+            printf(" ");
+        }
+        for( col=1;col<=2*row-1;col++)
+        {
+            printf("*",col);
+        }
+        printf("\n");
+    }
+    return 0;
+}
