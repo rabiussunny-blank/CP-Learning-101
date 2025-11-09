d32b8e7514fa691f48f6c2df89f1c7776c5157ab
Rabius $unny <rabiussunny7359@gmail.com>
2025-11-10 03:09:51 +0600

    Sheet 02 | N-Numbers_Histogram

---
+++ Assiut ICPC/Sheet 02/N_Numbers_Histogram.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    char S;
+    scanf("%c",&S);
+    int N;
+    scanf("%d",&N);
+    int x;
+    for(int i=1;i<=N;i++)
+    {
+        scanf("%d ",&x);
+
+    for(int j=1;j<=x;j++)
+    {
+        printf("%c",S);
+
+    }
+      printf("\n");
+    }
+
+     return 0;
+    }
+
+
+
+
