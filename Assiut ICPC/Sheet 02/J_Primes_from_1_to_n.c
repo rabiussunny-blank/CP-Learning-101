24b80575fa53b2ee8461a21de2f95fec2b7274ab
Rabius $unny <rabiussunny7359@gmail.com>
2025-11-10 03:05:09 +0600

    Sheet 02 | J-Primes_from_1_to_n

---
+++ Assiut ICPC/Sheet 02/J_Primes_from_1_to_n.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    for(int i=2;i<=N;i++)
+    {
+        int count=0;
+        for(int j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            printf("%d ",i);
+        }
+    }
+
+    return 0;
+}
